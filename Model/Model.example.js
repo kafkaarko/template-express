ff8427eb168b08d6775c85e4bde1e77e9d2be463
kafkaarko <kafkaarko@gmail.com>
2025-11-26 16:16:58 +0700

    template ready to use

--- Model/Model.example.js
+++ Model/Model.example.js
@@ -1,1 +1,30 @@
 // model use mongoose too create a schema and model and for primas cannot use this folder
+
+// const mongoose = require("mongoose");
+
+// const Schema = mongoose.Schema;
+
+// const userSchema = new Schema({
+//     name:{
+//         type: String,
+//         required: true
+//     },
+//     email:{
+//         type: String,
+//         required: true,
+//         unique: true,
+//         lowercase: true
+//     },
+//     password:{
+//         type: String,
+//         required: true
+//     },
+//     role:{
+//         type: String,
+//         enum: ["customer","admin","seller",'warehouse','courier'],
+//         default: "customer"
+//     }
+// }, {timestamps: true})
+
+// module.exports = mongoose.model("user",userSchema)
+// example mongoose model
